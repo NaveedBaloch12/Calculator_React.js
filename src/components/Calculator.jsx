a2bb871319d3eb29d41a73a4e52b31a30709f23a
NaveedBaloch12 <naveedhassankhan512@gmail.com>
2025-01-31 20:49:16 +0500

    Used map() function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -6,117 +6,168 @@ export default function Calculator() {
   const [NumInput, setNumInput] = React.useState(0);
   const [Operation, setOperation] = React.useState(null);
 
+  const BtnRow1 = ["DEL", "AC", "%", "/"];
+  const BtnRow2 = ["7", "8", "9", "*"];
+  const BtnRow3 = ["4", "5", "6", "-"];
+  const BtnRow4 = ["1", "2", "3", "+"];
+  const BtnRow5 = ["0", ".", "ANS", "="];
+
   // Implement all the operations here...
-  const HandleOperation = () => 
-  {
+  const HandleOperation = () => {
     // Add Operation..
-    if(Operation === "+")
-    {
+    if (Operation === "+") {
       setAnswer(Number(Answer) + Number(NumInput));
       setNumInput(0);
     }
     // Subtract Operation..
-    else if(Operation === "-")
-    {
+    else if (Operation === "-") {
       setAnswer(Number(Answer) - Number(NumInput));
       setNumInput(0);
     }
     // Multiply Operation..
-    else if(Operation === "*")
-    {
+    else if (Operation === "*") {
       setAnswer(Number(Answer) * Number(NumInput));
       setNumInput(0);
     }
     // Divide Operation..
-    else if(Operation === "/")
-    {
+    else if (Operation === "/") {
       setAnswer(Number(Answer) / Number(NumInput));
       setNumInput(0);
-    }
-    else
-    {
+    } else {
       setAnswer(Number(NumInput));
       setNumInput(0);
     }
     setNumInput(0);
-  }
+  };
 
-  const HandleClick = (e) => 
-  {
-    if(e.target.value === "=")
-    {
+  const HandleClick = (e) => {
+    if (e.target.value === "=") {
       HandleOperation();
-    }
-    else if(e.target.value === "+" || e.target.value === "-" || e.target.value === "*" || e.target.value === "/")
-    {
+    } else if (
+      e.target.value === "+" ||
+      e.target.value === "-" ||
+      e.target.value === "*" ||
+      e.target.value === "/"
+    ) {
       setOperation(e.target.value);
-      setAnswer(Answer === 0? Number(NumInput) : Answer);
-      if(NumInput !== 0)
-        HandleOperation();
+      setAnswer(Answer === 0 ? Number(NumInput) : Answer);
+      if (NumInput !== 0) HandleOperation();
       setNumInput(0);
-    }
-    else if(e.target.value === "AC") 
-    {
+    } else if (e.target.value === "AC") {
       setAnswer(0);
       setNumInput(0);
       setOperation(null);
-    }
-    else if(e.target.value === "DEL")
-    {
+    } else if (e.target.value === "DEL") {
       setNumInput(NumInput.toString().slice(0, -1));
       setOperation(null);
-    }
-    else if(e.target.value === "ANS")
-      {
-        setNumInput(Answer);
-      }
-    else
-    {
+    } else if (e.target.value === "ANS") {
+      setNumInput(Answer);
+    } else {
       setNumInput(NumInput === 0 ? e.target.value : NumInput + e.target.value);
     }
-  }
+  };
 
   return (
     <div className="d-flex flex-column justify-content-center align-items-center">
-        <h2 className="center">CALCULATOR</h2>
+      <h2 className="center">CALCULATOR</h2>
       <div className="calculator d-flex flex-column justify-content-center align-items-center card shadow p-3 mt-4">
-        <input value={Answer} type="text" name="Answer" className="display form-control" readOnly />
-        <input value={NumInput} type="text" className="display form-control mb-1" readOnly />
+        <input
+          value={Answer}
+          type="text"
+          name="Answer"
+          className="display form-control"
+          readOnly
+        />
+        <input
+          value={NumInput}
+          type="text"
+          className="display form-control mb-1"
+          readOnly
+        />
 
         <div className="container"></div>
         <div className="row justify-content-between w-100">
-          <button onClick={HandleClick} value={"DEL"} className="mybtn clrbtn col-sm">DEL</button>
-          <button onClick={HandleClick} value={"AC"} className="mybtn clrbtn col-sm">AC</button>
-          <button onClick={HandleClick} value={"%"} className="mybtn opbtn col-sm">%</button>
-          <button onClick={HandleClick} value={"/"} className="mybtn opbtn col-sm">÷</button> 
+          {
+            BtnRow1.map((btn, i) => {
+              return (
+                <button
+                  key={i}
+                  onClick={HandleClick}
+                  value={btn}
+                  className="mybtn col-sm"
+                >
+                  {btn}
+                </button>
+              );
+            })
+          }
         </div>
         <div className="row justify-content-between w-100">
-          <button onClick={HandleClick} value={7} className="mybtn col-sm">7</button>
-          <button onClick={HandleClick} value={8} className="mybtn col-sm">8</button> 
-          <button onClick={HandleClick} value={9} className="mybtn col-sm">9</button>
-          <button onClick={HandleClick} value={"*"} className="mybtn opbtn col-sm">×</button>
+        {
+            BtnRow2.map((btn, i) => {
+              return (
+                <button
+                  key={i}
+                  onClick={HandleClick}
+                  value={btn}
+                  className="mybtn col-sm"
+                >
+                  {btn}
+                </button>
+              );
+            })
+          }
         </div>
 
         <div className="row justify-content-between w-100">
-          <button onClick={HandleClick} value={4} className="mybtn col-sm">4</button>
-          <button onClick={HandleClick} value={5} className="mybtn col-sm">5</button>
-          <button onClick={HandleClick} value={6} className="mybtn col-sm">6</button>
-          <button onClick={HandleClick} value={"-"} className="mybtn opbtn col-sm">–</button>
-
+        {
+            BtnRow3.map((btn, i) => {
+              return (
+                <button
+                  key={i}
+                  onClick={HandleClick}
+                  value={btn}
+                  className="mybtn col-sm"
+                >
+                  {btn}
+                </button>
+              );
+            })
+          }
         </div>
 
         <div className="row justify-content-between w-100">
-          <button onClick={HandleClick} value={1} className="mybtn col-sm">1</button>
-          <button onClick={HandleClick} value={2} className="mybtn col-sm">2</button>
-          <button onClick={HandleClick} value={3} className="mybtn col-sm">3</button>
-          <button onClick={HandleClick} value={"+"} className="mybtn opbtn col-sm">+</button>
+        {
+            BtnRow4.map((btn, i) => {
+              return (
+                <button
+                  key={i}
+                  onClick={HandleClick}
+                  value={btn}
+                  className="mybtn col-sm"
+                >
+                  {btn}
+                </button>
+              );
+            })
+          }
         </div>
 
         <div className="row justify-content-between w-100">
-          <button onClick={HandleClick} value={0} className="mybtn col-sm">0</button>
-          <button onClick={HandleClick} value={"."} className="mybtn col-sm">.</button>
-          <button onClick={HandleClick} value={"ANS"} className="mybtn col-sm">ANS</button>
-          <button onClick={HandleClick} value={"="} className="mybtn opbtn col-sm">=</button>
+        {
+            BtnRow5.map((btn, i) => {
+              return (
+                <button
+                  key={i}
+                  onClick={HandleClick}
+                  value={btn}
+                  className="mybtn col-sm"
+                >
+                  {btn}
+                </button>
+              );
+            })
+          }
         </div>
       </div>
     </div>
